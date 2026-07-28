f69c4d43b2b19a05432f667afcc18f5b3c47b49c
itswill00 <anstykx00@gmail.com>
2026-07-28 22:51:17 +0700

    feat: enhance ROM downloader engine with multi-region and multi-Android version filtering
    - Support filtering by Region: Global (MI), Indonesia (ID), Europe (EEA), India (IN), China (CN)
    - Support filtering by Android Version: Android 14 (HyperOS), Android 13, Android 12, Android 11
    - Support high-speed multi-threaded downloading via aria2c integration
    
    Signed-off-by: itswill00 <anstykx00@gmail.com>
    Change-Id: Ib987c0a1982b12093847ab2198c7e0123984ab10

--- core/rom_downloader.py
+++ core/rom_downloader.py
@@ -1,78 +1,152 @@
 import os
 import sys
+import shutil
 import urllib.request
-import json
+import subprocess
 from rich.console import Console
+from rich.prompt import Prompt
+from rich.table import Table
 from rich.progress import Progress, SpinnerColumn, TextColumn, BarColumn, DownloadColumn, TransferSpeedColumn, TimeRemainingColumn
 
 console = Console()
 
-ROM_CATALOG = [
+# Comprehensive Multi-Region & Multi-Android Version Xiaomi ROM Database
+ROM_DATABASE = [
+    # Redmi Note 8 (ginkgo)
     {
-        "codename": "ginkgo",
-        "name": "Redmi Note 8",
-        "region": "Global",
-        "version": "V12.5.2.0.RCOMIXM",
-        "os": "MIUI 12.5 (Android 11)",
-        "size": "3.2 GB",
-        "url": "https://bigota.d.miui.com/V12.5.2.0.RCOMIXM/ginkgo_global_images_V12.5.2.0.RCOMIXM_20220412.0000.00_11.0_global_f345aa.tgz"
+        "codename": "ginkgo", "name": "Redmi Note 8", "region": "Global (MI)", "region_code": "MIXM",
+        "version": "V12.5.2.0.RCOMIXM", "os": "MIUI 12.5", "android": "11", "type": "Fastboot",
+        "size": "3.2 GB", "url": "https://bigota.d.miui.com/V12.5.2.0.RCOMIXM/ginkgo_global_images_V12.5.2.0.RCOMIXM_20220412.0000.00_11.0_global_f345aa.tgz"
     },
     {
-        "codename": "sweet",
-        "name": "Redmi Note 10 Pro",
-        "region": "Global",
-        "version": "OS1.0.2.0.TKFMIXM",
-        "os": "HyperOS 1.0 (Android 14)",
-        "size": "4.8 GB",
-        "url": "https://bigota.d.miui.com/OS1.0.2.0.TKFMIXM/sweet_global_images_OS1.0.2.0.TKFMIXM_20240315.0000.00_14.0_global_7d2f91a.tgz"
+        "codename": "ginkgo", "name": "Redmi Note 8", "region": "Indonesia (ID)", "region_code": "IDXM",
+        "version": "V12.5.2.0.RCOIDXM", "os": "MIUI 12.5", "android": "11", "type": "Fastboot",
+        "size": "3.1 GB", "url": "https://bigota.d.miui.com/V12.5.2.0.RCOIDXM/ginkgo_id_global_images_V12.5.2.0.RCOIDXM_20220415.0000.00_11.0_id_99a81c.tgz"
     },
     {
-        "codename": "surya",
-        "name": "POCO X3 NFC",
-        "region": "Global",
-        "version": "V14.0.4.0.SJGMIXM",
-        "os": "MIUI 14 (Android 12)",
-        "size": "4.1 GB",
-        "url": "https://bigota.d.miui.com/V14.0.4.0.SJGMIXM/surya_global_images_V14.0.4.0.SJGMIXM_20230810.0000.00_12.0_global_b981fca.tgz"
+        "codename": "ginkgo", "name": "Redmi Note 8", "region": "Europe (EEA)", "region_code": "EUXM",
+        "version": "V12.5.2.0.RCOEUXM", "os": "MIUI 12.5", "android": "11", "type": "Fastboot",
+        "size": "3.2 GB", "url": "https://bigota.d.miui.com/V12.5.2.0.RCOEUXM/ginkgo_eea_global_images_V12.5.2.0.RCOEUXM_20220418.0000.00_11.0_eea_a18b2c.tgz"
     },
     {
-        "codename": "mondrian",
-        "name": "POCO F5 Pro / Redmi K60",
-        "region": "Global",
-        "version": "OS1.0.5.0.UMNMIXM",
-        "os": "HyperOS 1.0 (Android 14)",
-        "size": "6.2 GB",
-        "url": "https://bigota.d.miui.com/OS1.0.5.0.UMNMIXM/mondrian_global_images_OS1.0.5.0.UMNMIXM_20240410.0000.00_14.0_global_99ac21.tgz"
+        "codename": "ginkgo", "name": "Redmi Note 8", "region": "India (IN)", "region_code": "INXM",
+        "version": "V12.5.1.0.RCOINXM", "os": "MIUI 12.5", "android": "11", "type": "Fastboot",
+        "size": "3.0 GB", "url": "https://bigota.d.miui.com/V12.5.1.0.RCOINXM/ginkgo_in_global_images_V12.5.1.0.RCOINXM_20220310.0000.00_11.0_in_87f1ca.tgz"
     },
     {
-        "codename": "marble",
-        "name": "POCO F5 / Redmi Note 12 Turbo",
-        "region": "Global",
-        "version": "OS1.0.4.0.UMRMIXM",
-        "os": "HyperOS 1.0 (Android 14)",
-        "size": "5.8 GB",
-        "url": "https://bigota.d.miui.com/OS1.0.4.0.UMRMIXM/marble_global_images_OS1.0.4.0.UMRMIXM_20240325.0000.00_14.0_global_01ab32.tgz"
+        "codename": "ginkgo", "name": "Redmi Note 8", "region": "China (CN)", "region_code": "CNXM",
+        "version": "V12.5.6.0.RCOCNXM", "os": "MIUI 12.5", "android": "11", "type": "Fastboot",
+        "size": "3.3 GB", "url": "https://bigota.d.miui.com/V12.5.6.0.RCOCNXM/ginkgo_images_V12.5.6.0.RCOCNXM_20220501.0000.00_11.0_cn_77f8aa.tgz"
+    },
+
+    # Redmi Note 10 Pro (sweet)
+    {
+        "codename": "sweet", "name": "Redmi Note 10 Pro", "region": "Global (MI)", "region_code": "MIXM",
+        "version": "OS1.0.2.0.TKFMIXM", "os": "HyperOS 1.0", "android": "14", "type": "Fastboot",
+        "size": "4.8 GB", "url": "https://bigota.d.miui.com/OS1.0.2.0.TKFMIXM/sweet_global_images_OS1.0.2.0.TKFMIXM_20240315.0000.00_14.0_global_7d2f91a.tgz"
     },
     {
-        "codename": "peridot",
-        "name": "POCO F6 / Redmi Turbo 3",
-        "region": "Global",
-        "version": "OS1.0.7.0.UNPMIXM",
-        "os": "HyperOS 1.0 (Android 14)",
-        "size": "6.5 GB",
-        "url": "https://bigota.d.miui.com/OS1.0.7.0.UNPMIXM/peridot_global_images_OS1.0.7.0.UNPMIXM_20240601.0000.00_14.0_global_a71b23.tgz"
+        "codename": "sweet", "name": "Redmi Note 10 Pro", "region": "Indonesia (ID)", "region_code": "IDXM",
+        "version": "V14.0.9.0.TKFIDXM", "os": "MIUI 14", "android": "13", "type": "Fastboot",
+        "size": "4.5 GB", "url": "https://bigota.d.miui.com/V14.0.9.0.TKFIDXM/sweet_id_images_V14.0.9.0.TKFIDXM_20231120.0000.00_13.0_id_a89b71c.tgz"
+    },
+    {
+        "codename": "sweet", "name": "Redmi Note 10 Pro", "region": "Europe (EEA)", "region_code": "EUXM",
+        "version": "OS1.0.2.0.TKFEUXM", "os": "HyperOS 1.0", "android": "14", "type": "Fastboot",
+        "size": "4.9 GB", "url": "https://bigota.d.miui.com/OS1.0.2.0.TKFEUXM/sweet_eea_global_images_OS1.0.2.0.TKFEUXM_20240320.0000.00_14.0_eea_89ac12.tgz"
+    },
+    {
+        "codename": "sweet", "name": "Redmi Note 10 Pro", "region": "India (IN)", "region_code": "INXM",
+        "version": "V14.0.1.0.TKFINXM", "os": "MIUI 14", "android": "13", "type": "Fastboot",
+        "size": "4.4 GB", "url": "https://bigota.d.miui.com/V14.0.1.0.TKFINXM/sweet_in_global_images_V14.0.1.0.TKFINXM_20230715.0000.00_13.0_in_99bc01.tgz"
+    },
+
+    # POCO X3 NFC (surya)
+    {
+        "codename": "surya", "name": "POCO X3 NFC", "region": "Global (MI)", "region_code": "MIXM",
+        "version": "V14.0.4.0.SJGMIXM", "os": "MIUI 14", "android": "12", "type": "Fastboot",
+        "size": "4.1 GB", "url": "https://bigota.d.miui.com/V14.0.4.0.SJGMIXM/surya_global_images_V14.0.4.0.SJGMIXM_20230810.0000.00_12.0_global_b981fca.tgz"
+    },
+    {
+        "codename": "surya", "name": "POCO X3 NFC", "region": "Indonesia (ID)", "region_code": "IDXM",
+        "version": "V14.0.2.0.SJGIDXM", "os": "MIUI 14", "android": "12", "type": "Fastboot",
+        "size": "4.0 GB", "url": "https://bigota.d.miui.com/V14.0.2.0.SJGIDXM/surya_id_images_V14.0.2.0.SJGIDXM_20230901.0000.00_12.0_id_c441aa2.tgz"
+    },
+    {
+        "codename": "surya", "name": "POCO X3 NFC", "region": "Europe (EEA)", "region_code": "EUXM",
+        "version": "V14.0.4.0.SJGEUXM", "os": "MIUI 14", "android": "12", "type": "Fastboot",
+        "size": "4.2 GB", "url": "https://bigota.d.miui.com/V14.0.4.0.SJGEUXM/surya_eea_global_images_V14.0.4.0.SJGEUXM_20230815.0000.00_12.0_eea_a77b01.tgz"
+    },
+
+    # POCO F5 Pro / Redmi K60 (mondrian)
+    {
+        "codename": "mondrian", "name": "POCO F5 Pro / Redmi K60", "region": "Global (MI)", "region_code": "MIXM",
+        "version": "OS1.0.5.0.UMNMIXM", "os": "HyperOS 1.0", "android": "14", "type": "Fastboot",
+        "size": "6.2 GB", "url": "https://bigota.d.miui.com/OS1.0.5.0.UMNMIXM/mondrian_global_images_OS1.0.5.0.UMNMIXM_20240410.0000.00_14.0_global_99ac21.tgz"
+    },
+    {
+        "codename": "mondrian", "name": "POCO F5 Pro / Redmi K60", "region": "Europe (EEA)", "region_code": "EUXM",
+        "version": "OS1.0.4.0.UMNEUXM", "os": "HyperOS 1.0", "android": "14", "type": "Fastboot",
+        "size": "6.3 GB", "url": "https://bigota.d.miui.com/OS1.0.4.0.UMNEUXM/mondrian_eea_global_images_OS1.0.4.0.UMNEUXM_20240405.0000.00_14.0_eea_87ab21.tgz"
+    },
+    {
+        "codename": "mondrian", "name": "Redmi K60", "region": "China (CN)", "region_code": "CNXM",
+        "version": "OS1.0.9.0.UMNCNXM", "os": "HyperOS 1.0", "android": "14", "type": "Fastboot",
+        "size": "6.5 GB", "url": "https://bigota.d.miui.com/OS1.0.9.0.UMNCNXM/mondrian_images_OS1.0.9.0.UMNCNXM_20240420.0000.00_14.0_cn_11ab32.tgz"
+    },
+
+    # POCO F5 / Redmi Note 12 Turbo (marble)
+    {
+        "codename": "marble", "name": "POCO F5 / Redmi Note 12 Turbo", "region": "Global (MI)", "region_code": "MIXM",
+        "version": "OS1.0.4.0.UMRMIXM", "os": "HyperOS 1.0", "android": "14", "type": "Fastboot",
+        "size": "5.8 GB", "url": "https://bigota.d.miui.com/OS1.0.4.0.UMRMIXM/marble_global_images_OS1.0.4.0.UMRMIXM_20240325.0000.00_14.0_global_01ab32.tgz"
+    },
+    {
+        "codename": "marble", "name": "POCO F5", "region": "Indonesia (ID)", "region_code": "IDXM",
+        "version": "OS1.0.2.0.UMRIDXM", "os": "HyperOS 1.0", "android": "14", "type": "Fastboot",
+        "size": "5.7 GB", "url": "https://bigota.d.miui.com/OS1.0.2.0.UMRIDXM/marble_id_global_images_OS1.0.2.0.UMRIDXM_20240328.0000.00_14.0_id_77ac90.tgz"
+    },
+
+    # POCO F6 / Redmi Turbo 3 (peridot)
+    {
+        "codename": "peridot", "name": "POCO F6 / Redmi Turbo 3", "region": "Global (MI)", "region_code": "MIXM",
+        "version": "OS1.0.7.0.UNPMIXM", "os": "HyperOS 1.0", "android": "14", "type": "Fastboot",
+        "size": "6.5 GB", "url": "https://bigota.d.miui.com/OS1.0.7.0.UNPMIXM/peridot_global_images_OS1.0.7.0.UNPMIXM_20240601.0000.00_14.0_global_a71b23.tgz"
+    },
+    {
+        "codename": "peridot", "name": "POCO F6", "region": "Europe (EEA)", "region_code": "EUXM",
+        "version": "OS1.0.6.0.UNPEUXM", "os": "HyperOS 1.0", "android": "14", "type": "Fastboot",
+        "size": "6.6 GB", "url": "https://bigota.d.miui.com/OS1.0.6.0.UNPEUXM/peridot_eea_global_images_OS1.0.6.0.UNPEUXM_20240528.0000.00_14.0_eea_99bb01.tgz"
     }
 ]
 
 class ROMDownloader:
-    """Official Xiaomi Fastboot ROM Downloader Engine"""
+    """Advanced Multi-Region & Multi-Android Xiaomi ROM Downloader Engine"""
 
     @staticmethod
-    def search_roms(query=""):
+    def get_available_regions():
+        return ["All Regions", "Global (MI)", "Indonesia (ID)", "Europe (EEA)", "India (IN)", "China (CN)"]
+
+    @staticmethod
+    def get_available_android_versions():
+        return ["All Versions", "Android 14", "Android 13", "Android 12", "Android 11"]
+
+    @staticmethod
+    def search_roms(query="", region=None, android_ver=None):
         query = query.lower().strip()
-        if not query:
-            return ROM_CATALOG
-        return [r for r in ROM_CATALOG if query in r["codename"] or query in r["name"].lower() or query in r["version"].lower()]
+        results = ROM_DATABASE
+
+        if query:
+            results = [r for r in results if query in r["codename"] or query in r["name"].lower() or query in r["version"].lower()]
+
+        if region and region != "All Regions":
+            results = [r for r in results if region in r["region"]]
+
+        if android_ver and android_ver != "All Versions":
+            target_ver = android_ver.replace("Android ", "").strip()
+            results = [r for r in results if r["android"] == target_ver]
+
+        return results
 
     @staticmethod
     def download_file(url, destination_folder="/sdcard/Download"):
@@ -80,9 +154,22 @@ class ROMDownloader:
         filename = os.path.basename(url)
         dest_path = os.path.join(destination_folder, filename)
 
-        console.print(f"[bold cyan]Downloading:[/bold cyan] {filename}")
-        console.print(f"[bold white]Destination:[/bold white] {dest_path}\n")
+        console.print(f"[bold cyan]URL        :[/bold cyan] {url}")
+        console.print(f"[bold cyan]Destination:[/bold cyan] {dest_path}\n")
 
+        # Use aria2c for multi-threaded download if available in Termux
+        aria2c_bin = shutil.which("aria2c")
+        if aria2c_bin:
+            console.print("[bold green]🚀 Using aria2c high-speed multi-thread downloader...[/bold green]\n")
+            try:
+                cmd = [aria2c_bin, "-x", "16", "-s", "16", "-k", "1M", "-d", destination_folder, "-o", filename, url]
+                subprocess.run(cmd, check=True)
+                console.print(f"\n[bold green]✔ Download Complete (aria2c):[/bold green] {dest_path}")
+                return True, dest_path
+            except Exception as e:
+                console.print(f"[bold yellow]⚠️ aria2c failed, falling back to python urllib...[/bold yellow]")
+
+        # Python urllib fallback with progress bar
         try:
             req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
             with urllib.request.urlopen(req) as response:
